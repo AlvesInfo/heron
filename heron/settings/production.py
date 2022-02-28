605ecedd918e39ce8f2f856ae598b380a4cabecc
AlvesInfo <paulo.alves@4a-info.fr>
2022-02-28 18:40:54 +0100

    commit de la structure des apps et des models parameters

--- heron/settings/production.py
+++ heron/settings/production.py
@@ -13,22 +13,22 @@ INSTALLED_APPS += THIRD_PARTY_APPS
 
 MIDDLEWARE = [] + MIDDLEWARE + ["axes.middleware.AxesMiddleware"]
 
-# STATIC_URL = "/static/"
-#
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = Path(BASE_DIR, "files/media").resolve()
-#
-# SESSION_COOKIE_AGE = 36000
-# SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = Path(BASE_DIR, "files/media").resolve()
+
+SESSION_COOKIE_AGE = 36000
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # SECURE_CONTENT_TYPE_NOSNIFF = True
 # SECURE_BROWSER_XSS_FILTER = True
 # X_FRAME_OPTIONS = "DENY"
 
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# heron_DIR_LOG = str(LOG_DIR) if DEBUG else "/var/log/heron"
+heron_DIR_LOG = str(LOG_DIR) if DEBUG else "/var/log/heron"
 
 AUTHENTICATION_BACKENDS = [
     # AxesBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
@@ -39,11 +39,11 @@ AUTHENTICATION_BACKENDS = [
 
 # AXES lockout responses on failed user authentication attempts from login views
 # https://django-axes.readthedocs.io/en/latest/4_configuration.html
-# AXES_ENABLED = True
-# AXES_FAILURE_LIMIT = 5
-# AXES_ONLY_USER_FAILURES = True
-# AXES_LOCKOUT_TEMPLATE = 'axes_blocked.html'
-# AXES_USERNAME_FORM_FIELD = "email"
+AXES_ENABLED = True
+AXES_FAILURE_LIMIT = 5
+AXES_ONLY_USER_FAILURES = True
+AXES_LOCKOUT_TEMPLATE = 'axes_blocked.html'
+AXES_USERNAME_FORM_FIELD = "email"
 
 # LOGGING = {
 #     "version": 1,
@@ -120,13 +120,13 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [] + MIDDLEWARE + []
 
-# LOGIN_REDIRECT_URL = "/"
-# LOGOUT_REDIRECT_URL = "accounts/logout/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "accounts/logout/"
 
 # DJANGO-CLAMD configuration
 # https://pypi.org/project/django-clamd/
-# CLAMD_ENABLED = True
-# CLAMD_SOCKET = "/var/run/clamd/clamd.ctl"
-# CLAMD_USE_TCP = False
-# CLAMD_TCP_SOCKET = 3310
-# CLAMD_TCP_ADDR = "127.0.0.1"
+CLAMD_ENABLED = True
+CLAMD_SOCKET = "/var/run/clamd/clamd.ctl"
+CLAMD_USE_TCP = False
+CLAMD_TCP_SOCKET = 3310
+CLAMD_TCP_ADDR = "127.0.0.1"
