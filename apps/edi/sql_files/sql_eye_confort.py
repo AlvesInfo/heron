3bed996ba19fa2c5045e13f5eef9890a421704d7
AlvesInfo <paulo.alves@4a-info.fr>
2022-12-26 22:28:17 +0100

    changement des imports accountancy pourles regimes de taxes

--- apps/edi/sql_files/sql_eye_confort.py
+++ apps/edi/sql_files/sql_eye_confort.py
@@ -24,7 +24,7 @@ post_eye_dict = {
             "vat_rate" = 0,
             "vat_amount" = 0,
             "amount_with_vat" = "net_amount"::numeric
-        where "uuid_identification" = %(uuid_identification)
+        where "uuid_identification" = %(uuid_identification)s
         and ("valid" = false or "valid" isnull
         """
     ),
