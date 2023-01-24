5413e36b9b5a8e188a014137fb2681ccf63974bf
AlvesInfo <paulo.alves@4a-info.fr>
2023-01-24 07:11:48 +0100

    ajout des exclusions Tiers X3/Maison et Tiers /Pays Maisons

--- heron/middleware/login_middleware.py
+++ heron/middleware/login_middleware.py
@@ -32,9 +32,31 @@ class LoginMiddleware:
         :param request: request django
         :return: Redirige vers la page de login si non connecté.
         """
+        list_path = str(request.path).split("/")
+        # on vérifie password-reset-confirm dans request.path pour les reset des passwords
+        is_confirm = list_path[1] == "password-reset-confirm"
 
-        if not request.user.is_authenticated and request.path != '/accounts/login/':
-            return redirect("accounts:login")
+        # on vérifie reactivate dans request.path pour les reset des passwords
+        if len(list_path) >= 2 and f"{list_path[1]}" == "reactivate":
+            is_confirm = True
+
+        # Si l'on est pas authentifié, on est renvoyé sur la page de login
+        paths = [
+            "/logout_email/",
+            "/password-reset/",
+            "/password-reset-done/",
+            "/password-reset-complete/",
+            "/accounts/login/",
+            str(request.path) if is_confirm else "",
+        ]
+
+        if not request.user.is_authenticated:
+            if request.path not in paths:
+                return redirect("accounts:login")
+
+        # Avant changements
+        # if not request.user.is_authenticated and request.path != '/accounts/login/':
+        #     return redirect("accounts:login")
 
         response = self.get_response(request)
 
