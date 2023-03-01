c7a44d0fc591ee1aa870cf6ebf527d0d9370e6cc
AlvesInfo <paulo.alves@4a-info.fr>
2023-03-01 12:26:50 +0100

    ajouts des axes par familles statistiques tiers

--- apps/centers_clients/views/__init__.py
+++ apps/centers_clients/views/__init__.py
@@ -1,3 +1,4 @@
 from apps.centers_clients.views.maisons_views import *
 from apps.centers_clients.views.exclusions_tiers_clients_views import *
 from apps.centers_clients.views.exclusions_tiers_pays_clients_views import *
+from apps.centers_clients.views.maisons_subscriptions import *
