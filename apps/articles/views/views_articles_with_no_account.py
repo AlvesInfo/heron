db305202cbcd4ea47610985d6f2343ed498e4f5a
AlvesInfo <paulo.alves@4a-info.fr>
2023-05-29 11:45:26 +0200

    déplacement du champs email client

--- apps/articles/views/views_articles_with_no_account.py
+++ apps/articles/views/views_articles_with_no_account.py
@@ -10,7 +10,6 @@ from django.db.models.functions import Coalesce, Cast
 from apps.core.functions.functions_http import get_pagination_buttons
 from apps.articles.models import Article
 from apps.edi.models import EdiImport
-from apps.book.models import Society
 from apps.centers_purchasing.models import AccountsAxeProCategory
 
 
@@ -21,52 +20,48 @@ def articles_without_account_list(request):
     """
     limit = 200
 
-    paginator = Paginator(
-        (
-            EdiImport.objects.annotate(
-                supplierm=Max("supplier"),
-                libellem=Max("libelle"),
-                sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
-            )
-            .annotate(
-                without=Exists(
-                    AccountsAxeProCategory.objects.annotate(
-                        sub_categoryc=Coalesce(
-                            Cast("sub_category", output_field=CharField()), Value("")
-                        ),
-                    ).filter(
-                        child_center=OuterRef("code_center"),
-                        axe_pro=OuterRef("axe_pro"),
-                        big_category=OuterRef("big_category"),
-                        sub_categoryc=OuterRef("sub_categoryc"),
-                        vat=OuterRef("vat"),
-                    )
-                ),
-                article=Article.objects.filter(
-                    third_party_num=OuterRef("third_party_num"),
-                    reference=OuterRef("reference_article"),
-                ).values("pk"),
-            )
-            .annotate()
-            .values(
-                "code_center",
-                "third_party_num",
-                "supplierm",
-                "reference_article",
-                "libellem",
-                "axe_pro__section",
-                "big_category__name",
-                "sub_category__name",
-                "vat",
-                "article",
-            )
-            .exclude(without=True)
-            .annotate(Count("id"))
-            .order_by("code_center", "third_party_num", "reference_article")
-        ),
-        limit,
+    queryset = (
+        EdiImport.objects.annotate(
+            supplierm=Max("supplier"),
+            libellem=Max("libelle"),
+            sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
+        )
+        .annotate(
+            without=Exists(
+                AccountsAxeProCategory.objects.annotate(
+                    sub_categoryc=Coalesce(
+                        Cast("sub_category", output_field=CharField()), Value("")
+                    ),
+                ).filter(
+                    child_center=OuterRef("code_center"),
+                    axe_pro=OuterRef("axe_pro"),
+                    big_category=OuterRef("big_category"),
+                    sub_categoryc=OuterRef("sub_categoryc"),
+                    vat=OuterRef("vat"),
+                )
+            ),
+            article=Article.objects.filter(
+                third_party_num=OuterRef("third_party_num"),
+                reference=OuterRef("reference_article"),
+            ).values("pk"),
+        )
+        .annotate()
+        .values(
+            "code_center",
+            "third_party_num",
+            "supplierm",
+            "reference_article",
+            "libellem",
+            "axe_pro__section",
+            "big_category__name",
+            "sub_category__name",
+            "vat",
+            "article",
+        )
+        .exclude(without=True)
+        .annotate(Count("id"))
     )
-
+    paginator = Paginator(queryset, limit)
     page = request.GET.get("page")
 
     try:
@@ -75,6 +70,15 @@ def articles_without_account_list(request):
         articles = paginator.page(1)
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
+
+    count = queryset.count()
+    titre_count = ""
+
+    if count == 1:
+        titre_count = " (1 article sans comptes)"
+
+    if count > 1:
+        titre_count = f" ({str(count)} articles sans comptes)"
 
     context = {
         "articles": articles,
@@ -85,7 +89,7 @@ def articles_without_account_list(request):
         "num_pages": paginator.num_pages,
         "start_index": (articles.start_index() - 1) if articles.start_index() else 0,
         "end_index": articles.end_index(),
-        "titre_table": "1.2 - Articles sans comptes",
+        "titre_table": f"1.2 - Articles sans comptes{titre_count}",
         "url_validation": reverse("articles:articles_new_validation"),
         "url_redirect": reverse("articles:new_articles_list"),
     }
