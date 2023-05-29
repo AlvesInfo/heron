0b7bba308cddbf79a7d9859d7f73d031fc67cfb2
AlvesInfo <paulo.alves@4a-info.fr>
2023-05-29 11:35:55 +0200

    déplacement du champs email client

--- apps/articles/views/views_articles_with_no_account.py
+++ apps/articles/views/views_articles_with_no_account.py
@@ -21,49 +21,52 @@ def articles_without_account_list(request):
     """
     limit = 200
 
-    queryset = (
-        EdiImport.objects.annotate(
-            supplierm=Max("supplier"),
-            libellem=Max("libelle"),
-            sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
-        )
-        .annotate(
-            without=Exists(
-                AccountsAxeProCategory.objects.annotate(
-                    sub_categoryc=Coalesce(
-                        Cast("sub_category", output_field=CharField()), Value("")
-                    ),
-                ).filter(
-                    child_center=OuterRef("code_center"),
-                    axe_pro=OuterRef("axe_pro"),
-                    big_category=OuterRef("big_category"),
-                    sub_categoryc=OuterRef("sub_categoryc"),
-                    vat=OuterRef("vat"),
-                )
-            ),
-            article=Article.objects.filter(
-                third_party_num=OuterRef("third_party_num"), reference=OuterRef("reference_article")
-            ).values("pk"),
-        )
-        .annotate()
-        .values(
-            "code_center",
-            "third_party_num",
-            "supplierm",
-            "reference_article",
-            "libellem",
-            "axe_pro__section",
-            "big_category__name",
-            "sub_category__name",
-            "vat",
-            "article",
-        )
-        .exclude(without=True)
-        .annotate(Count("id"))
-        .order_by("code_center", "third_party_num", "reference_article")
+    paginator = Paginator(
+        (
+            EdiImport.objects.annotate(
+                supplierm=Max("supplier"),
+                libellem=Max("libelle"),
+                sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
+            )
+            .annotate(
+                without=Exists(
+                    AccountsAxeProCategory.objects.annotate(
+                        sub_categoryc=Coalesce(
+                            Cast("sub_category", output_field=CharField()), Value("")
+                        ),
+                    ).filter(
+                        child_center=OuterRef("code_center"),
+                        axe_pro=OuterRef("axe_pro"),
+                        big_category=OuterRef("big_category"),
+                        sub_categoryc=OuterRef("sub_categoryc"),
+                        vat=OuterRef("vat"),
+                    )
+                ),
+                article=Article.objects.filter(
+                    third_party_num=OuterRef("third_party_num"),
+                    reference=OuterRef("reference_article"),
+                ).values("pk"),
+            )
+            .annotate()
+            .values(
+                "code_center",
+                "third_party_num",
+                "supplierm",
+                "reference_article",
+                "libellem",
+                "axe_pro__section",
+                "big_category__name",
+                "sub_category__name",
+                "vat",
+                "article",
+            )
+            .exclude(without=True)
+            .annotate(Count("id"))
+            .order_by("code_center", "third_party_num", "reference_article")
+        ),
+        limit,
     )
 
-    paginator = Paginator(queryset, limit)
     page = request.GET.get("page")
 
     try:
