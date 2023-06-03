3352d35a78ce62a45d3fd58715d27487b28390e4
AlvesInfo <paulo.alves@4a-info.fr>
2023-06-03 22:22:47 +0200

    déplacement du champs email client

--- apps/articles/parameters/querysets.py
+++ apps/articles/parameters/querysets.py
@@ -9,61 +9,11 @@ created by: Paulo ALVES
 modified at: 2022-05-29
 modified by: Paulo ALVES
 """
-from django.db.models import Max, Value, CharField, Exists, OuterRef, Count, Case, When, Q
+from django.db.models import Max, Value, CharField, Exists, OuterRef, Count, Case, When, Q, F
 from django.db.models.functions import Coalesce, Cast
 
 from apps.articles.models import Article, ArticleAccount
 from apps.edi.models import EdiImport
-from apps.centers_purchasing.models import AccountsAxeProCategory
-
-
-articles_without_account_queryset = (
-    EdiImport.objects.annotate(
-        supplierm=Max("supplier"),
-        libellem=Max("libelle"),
-        sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
-    )
-    .annotate(
-        without=Exists(
-            AccountsAxeProCategory.objects.annotate(
-                sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
-            ).filter(
-                child_center=OuterRef("code_center"),
-                axe_pro=OuterRef("axe_pro"),
-                big_category=OuterRef("big_category"),
-                sub_categoryc=OuterRef("sub_categoryc"),
-                vat=OuterRef("vat"),
-            )
-        ),
-        article=Article.objects.filter(
-            third_party_num=OuterRef("third_party_num"),
-            reference=OuterRef("reference_article"),
-        ).values("pk"),
-    )
-    .annotate()
-    .values(
-        "code_center",
-        "third_party_num",
-        "supplierm",
-        "reference_article",
-        "libellem",
-        "axe_pro__section",
-        "big_category__name",
-        "sub_category__name",
-        "vat",
-        "article",
-    )
-    .exclude(without=True)
-    .annotate(Count("id"))
-    .order_by(
-        "code_center",
-        "third_party_num",
-        "supplierm",
-        "reference_article",
-        "big_category__name",
-        "vat",
-    )
-)
 
 
 articles_with_account_queryset = (
@@ -97,6 +47,68 @@ articles_with_account_queryset = (
         "article__third_party_num__short_name",
         "article__reference",
         "article__big_category__name",
+        "vat",
+    )
+)
+
+articles_accounts = Article.objects.annotate(
+    code_center=F("account_article__child_center"),
+    vat=F("account_article__vat")
+).values(
+    "third_party_num",
+    "reference",
+    "code_center",
+    "vat"
+).filter(
+    third_party_num=OuterRef("third_party_num"),
+    reference=OuterRef("reference_article"),
+    code_center=OuterRef("code_center"),
+    vat=OuterRef("vat"),
+)
+
+articles_without_account_queryset = (
+    EdiImport.objects.annotate(
+        supplierm=Max("supplier"),
+        libellem=Max("libelle"),
+        sub_categoryc=Coalesce(Cast("sub_category", output_field=CharField()), Value("")),
+    )
+    .annotate(
+        without=Exists(
+            Article.objects.annotate(
+                code_center=F("account_article__child_center"),
+                vat=F("account_article__vat")
+            ).values(
+                "third_party_num",
+                "reference",
+                "code_center",
+                "vat"
+            ).filter(
+                third_party_num=OuterRef("third_party_num"),
+                reference=OuterRef("reference_article"),
+                code_center=OuterRef("code_center"),
+                vat=OuterRef("vat"),
+            )
+        ),
+    )
+    .values(
+        "code_center",
+        "third_party_num",
+        "supplierm",
+        "reference_article",
+        "libellem",
+        "axe_pro__section",
+        "big_category__name",
+        "sub_category__name",
+        "vat",
+    )
+    .exclude(without=True)
+    .annotate(Count("id"))
+    .order_by(
+        "code_center",
+        "third_party_num",
+        "supplierm",
+        "reference_article",
+        "big_category__name",
         "vat",
     )
 )
