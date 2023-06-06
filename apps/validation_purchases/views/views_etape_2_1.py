fc9db34d7f50f547d4e555a9b43dfe2def0359aa
AlvesInfo <paulo.alves@4a-info.fr>
2023-06-06 13:51:40 +0200

    déplacement du champs email client

--- apps/validation_purchases/views/views_etape_2_1.py
+++ apps/validation_purchases/views/views_etape_2_1.py
@@ -213,6 +213,13 @@ class UpdateIntegrationControl(ChangeTraceMixin, SuccessMessageMixin, UpdateView
         """Ajout de l'user à la sauvegarde du formulaire"""
         form.instance.modified_by = self.request.user
         self.request.session["level"] = 20
+        instance = form.save()
+        EdiImport.objects.filter(
+            third_party_num=self.third_party_num,
+            supplier=self.supplier,
+            invoice_month=self.invoice_month,
+            valid=True,
+        ).update(uuid_control=instance.uuid_identification)
 
         return super().form_valid(form)
 
