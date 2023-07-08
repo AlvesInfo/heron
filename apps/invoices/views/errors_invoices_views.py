1c8a04983f2572330a7a58558a6bc304870169d4
AlvesInfo <paulo.alves@4a-info.fr>
2023-07-08 11:45:11 +0200

    mise en place de la generation des factures pdf par celery

---
+++ apps/invoices/views/errors_invoices_views.py
@@ -0,0 +1,14 @@
+# pylint: disable=E0401
+"""
+Views pour réafecter une mauvaise affectaion sur une facture, précédente, soit un mauvais code
+de la part du fournisseur ou une mauvaise affectation de l'opérateur qui a effectué
+la facturationion précédente.
+
+Commentaire:
+
+created at: 2023-07-07
+created by: Paulo ALVES
+
+modified at: 2023-07-07
+modified by: Paulo ALVES
+"""
--- heron/processors.py
+++ heron/processors.py
@@ -1,3 +1,4 @@
+# pylint: disable=E0401
 """
 Module context_processors de django, pour alimenter l'ensemble des templates
     Date de création : 02/11/2019
@@ -17,6 +18,7 @@ from apps.users.models import AuthGroupName
 
 
 def debug(request):
+    """For debug"""
     if request:
         ...
     return {"environnement_debug": settings.DEBUG}
@@ -24,8 +26,8 @@ def debug(request):
 
 def title(request):
     """Retourne le dictionnaire de context avec le titre de l'application
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     dic = {}
 
@@ -37,8 +39,8 @@ def title(request):
 
 def user_group(request):
     """Retourne le dictionnaire de context du groupe auquel appartient l'user
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     dic = {"user_group": tuple(request.user.groups.values_list("name", flat=True))}
     return dic
@@ -46,8 +48,8 @@ def user_group(request):
 
 def groups_processor(request):
     """Retourne le dictionnaire de context du nom des groupes
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     dic = {"groups": []}
 
@@ -60,13 +62,13 @@ def groups_processor(request):
 
 def groupes_processor(request):
     """Retourne le dictionnaire de context des groupes auquel appartient l'user
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     if request.user.is_superuser:
-        rows_groupes = AuthGroupName.objects.exclude(
-            group__name__iexact="Admin"
-        ).values_list("group__name", "group_name")
+        rows_groupes = AuthGroupName.objects.exclude(group__name__iexact="Admin").values_list(
+            "group__name", "group_name"
+        )
 
     elif str(request.user) == "AnonymousUser":
         rows_groupes = {}
@@ -78,7 +80,7 @@ def groupes_processor(request):
             .values_list("group__name", "group_name")
         )
 
-    groupes = {g: n for g, n in rows_groupes}
+    groupes = dict(rows_groupes)
     groupes = OrderedDict(sorted(groupes.items(), key=lambda t: t[1]))
     dic = {"groupes": groupes}
     templates_path = Path(settings.PROJECT_DIR) / "templates/heron"
@@ -98,7 +100,14 @@ def in_acuitis(reqquest):
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_acuitis": "ACF" in centers_list or "*" in centers_list}
 
 
@@ -108,17 +117,31 @@ def in_ari(reqquest):
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_ari": "ARI" in centers_list or "*" in centers_list}
 
 
 def in_do(reqquest):
     """
-    Retoune True si l'user appartient à la centrale fille DO
+    Retoune True si l'user appartient à la centrale fille ARI
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_do": "DO" in centers_list or "*" in centers_list}
 
 
@@ -128,7 +151,14 @@ def in_ga(reqquest):
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_ga": "GAF" in centers_list or "*" in centers_list}
 
 
@@ -138,7 +168,14 @@ def in_maa(reqquest):
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_maa": "MAA" in centers_list or "*" in centers_list}
 
 
@@ -148,14 +185,21 @@ def in_unisson(reqquest):
     :param reqquest: request au sens django
     :return: bool
     """
-    centers_list = reqquest.user.code_child_center.split("|")
+    centers_list = []
+
+    try:
+        centers_list = reqquest.user.code_child_center.split("|")
+    except AttributeError:
+        # 'AnonymousUser' object has no attribute 'code_child_center'
+        ...
+
     return {"in_unisson": "UNI" in centers_list or "*" in centers_list}
 
 
 def date_du_jour(request):
     """Retourne le dictionnaire de context la date du jour
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     dic = {}
 
@@ -167,8 +211,8 @@ def date_du_jour(request):
 
 def annee_du_jour(request):
     """Retourne le dictionnaire de context l'année de la date du jour
-        :param request: object request
-        :return: context dict
+    :param request: object request
+    :return: context dict
     """
     dic = {}
 
@@ -178,16 +222,16 @@ def annee_du_jour(request):
     return dic
 
 
-def domain_site(request):
+def domain_site(_):
     """Retourne le domaine du site
-        :return: context dict
+    :return: context dict
     """
     return {"domain_site": f"http://{Site.objects.get_current().domain}"}
 
 
-def domain_debug(request):
+def domain_debug(_):
     """Retourne le domaine du site
-        :return: context dict
+    :return: context dict
     """
     return {"domain_debug": "http://127.0.0.1:8000"}
 
