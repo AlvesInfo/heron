f8f06040695882afa298655667e5219337289113
AlvesInfo <paulo.alves@4a-info.fr>
2024-04-12 19:28:59 +0200

    changement familles ZTRANSFERT

--- heron/celery.py
+++ heron/celery.py
@@ -8,13 +8,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heron.settings')
 
 BROKER_URL = f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
 
-app = Celery('heron', broker=BROKER_URL)
+app = Celery(settings.BASE_DIR.name, broker=BROKER_URL)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace=f'CELERY_{settings.BASE_DIR.name}')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
