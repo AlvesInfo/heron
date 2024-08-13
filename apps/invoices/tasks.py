ec8e054744c2b28b03f399a78e9616fbd0c21203
AlvesInfo <paulo.alves@4a-info.fr>
2024-08-13 17:23:29 +0200

    copy statics

--- apps/invoices/tasks.py
+++ apps/invoices/tasks.py
@@ -281,7 +281,8 @@ def send_invoice_email(context_dict: Dict, user_pk: int):
 
     try:
         user = User.objects.get(pk=user_pk)
-        with smtplib.SMTP_SSL(EMAIL_HOST, 465) as server:
+        with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+            server.starttls()
             server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
             trace, to_print = invoices_send_by_email(server, context_dict)
         trace.created_by = user
@@ -457,8 +458,7 @@ def send_invoice_email_essais(context_dict: Dict, user_pk: int):
 
         with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
             server.starttls()
-            server.ehlo("comptabilite@acuitis.com")
-            # server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+            server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
             trace, to_print = essais_send_by_email(server, context_dict)
 
         trace.created_by = user
