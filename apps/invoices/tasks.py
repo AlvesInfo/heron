dd6eb92ac203c1135f9b424cece435e0d3123f2d
AlvesInfo <paulo.alves@4a-info.fr>
2024-08-13 18:14:20 +0200

    copy statics

--- apps/invoices/tasks.py
+++ apps/invoices/tasks.py
@@ -458,6 +458,7 @@ def send_invoice_email_essais(context_dict: Dict, user_pk: int):
 
     try:
         user = User.objects.get(pk=user_pk)
+        time.sleep(1)
 
         with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
             server.starttls()
