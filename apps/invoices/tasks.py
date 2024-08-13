9b16564cce4f3552c77951551dc4cb64d9abd1b3
AlvesInfo <paulo.alves@4a-info.fr>
2024-08-13 16:52:15 +0200

    copy statics

--- apps/invoices/tasks.py
+++ apps/invoices/tasks.py
@@ -457,7 +457,7 @@ def send_invoice_email_essais(context_dict: Dict, user_pk: int):
 
         with smtplib.SMTP_SSL(EMAIL_HOST, 465) as server:
             server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-            server.ehlo("acuitis.com")
+            server.ehlo("comptabilite@acuitis.com")
             trace, to_print = essais_send_by_email(server, context_dict)
 
         trace.created_by = user
