2141fa11066119b8b86c79757ad193678a87b656
AlvesInfo <paulo.alves@4a-info.fr>
2024-09-13 22:12:32 +0200

    change trace delete

--- apps/core/bin/emails.py
+++ apps/core/bin/emails.py
@@ -10,20 +10,19 @@ created by: Paulo ALVES
 modified at: 2023-06-13
 modified by: Paulo ALVES
 """
-
 import smtplib
+import ssl
 from pathlib import Path
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email import encoders
 from email.mime.base import MIMEBase
-from threading import Lock
-from typing import Union
 
 import dkim
 from bs4 import BeautifulSoup
 
 from apps.core.functions.functions_setups import settings
+from apps.core.functions.functions_utilitaires import iter_slice
 from apps.core.exceptions import EmailException
 from heron.loggers import LOGGER_EMAIL
 
@@ -36,101 +35,69 @@ DOMAIN = settings.DOMAIN
 DKIM_PEM_FILE = settings.DKIM_PEM_FILE
 
 
-class SingletonMeta(type):
-    """
-    This is a thread-safe implementation of Singleton.
-    """
+class SmtpServer:
+    _instance = None
 
-    _instances = {}
-    _nb_instances = 0
-    _lock: Lock = Lock()
-    """
-    We now have a lock object that will be used to synchronize threads during
-    first access to the Singleton.
-    """
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SmtpServer, cls).__new__(cls, *args, **kwargs)
 
-    def __call__(cls, *args, **kwargs):
-        """
-        Possible changes to the value of the `__init__` argument do not affect
-        the returned instance.
-        """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
-        with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
+        return cls._instance
 
-        cls._nb_instances += 1
-
-        return cls._instances[cls]
-
-
-class SmtpServer(metaclass=SingletonMeta):
-    """Singleton du serveur de mail"""
-
-    server_mail: Union[smtplib.SMTP, smtplib.SMTP_SSL] = None
-
-    def __init__(self):
-        """Initialisation"""
-        self.__enter__()
+    def __init__(
+        self,
+        host: str = EMAIL_HOST,
+        port: int = EMAIL_PORT,
+        username: str = EMAIL_HOST_USER,
+        password: str = EMAIL_HOST_PASSWORD,
+        cls_smtp: smtplib.SMTP = smtplib.SMTP,
+        use_starttls: bool = True,
+        **kwargs,
+    ):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.use_starttls = use_starttls
+        self.cls_smtp = cls_smtp
+        self.kws_smtp = kwargs or {}
+        self.connection = None
 
     def __enter__(self):
-        """Context manager à l'entrée"""
         self.connect()
-        return self
 
-    def __exit__(self, *args, **kwargs):
-        """Context manager à la sortie"""
-        if self.__class__._nb_instances > 0:
-            self.__class__._nb_instances -= 1
+    def __exit__(self, *args):
+        self.close()
 
-        if self.__class__._nb_instances == 0 and self.server_mail is not None:
-            self.server_mail.quit()
-            self.server_mail = None
+    def connect(self):
+        """Connect to the SMTP Server"""
+        self.connection = self.get_server()
 
-    def re_connect(self):
-        """Reconnexion au serveur SMTP"""
+    def close(self):
+        """Close (quit) the connection"""
+        if self.connection:
+            self.connection.quit()
+            self.connection = None
 
-        if settings.EMAIL_USE_SSL:
-            self.server_mail = smtplib.SMTP_SSL(
-                settings.EMAIL_HOST, settings.EMAIL_PORT
-            )
-        else:
-            self.server_mail = smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+    def get_server(self) -> smtplib.SMTP:
+        """Connect and get the SMTP Server"""
+        user = self.username
+        password = self.password
 
-        if settings.EMAIL_USE_TLS:
-            self.server_mail.starttls()
+        server = self.cls_smtp(self.host, self.port, **self.kws_smtp)
 
-        test = self.server_mail.ehlo("acuitis.com")[0]
+        if self.use_starttls:
+            server.starttls()
 
-        if test == 250:
-            self.server_mail.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+        if user is not None or password is not None:
+            server.login(user, password)
 
-    def connect(self, force: bool = False):
-        """Connection au serveur SMTP
-        :param force: pour forcer une connection après
-        """
-        if self.server_mail is None or force:
-            self.re_connect()
+        return server
 
-    def server(self):
-        self.connect()
-        return self.server_mail
-
-    def quit(self):
-        self.__exit__()
-
-    def nb_connect(self):
-        return self.__class__._nb_instances
+    @property
+    def is_alive(self):
+        """bool: Check if there is a connection to the SMTP server"""
+        return self.connection is not None
 
 
 def prepare_mail(message, body, subject, email_text="", email_html="", context=None):
@@ -140,9 +107,7 @@ def prepare_mail(message, body, subject, email_text="", email_html="", context=N
 
     subject_mail = subject.format(**context)
     translate_email_html = email_html.format(**context)
-    translate_email_text = (
-        BeautifulSoup(translate_email_html, "lxml").get_text() or email_text
-    )
+    translate_email_text = BeautifulSoup(translate_email_html, "lxml").get_text() or email_text
 
     message["From"] = EMAIL_HOST_USER
     message["Subject"] = subject_mail
@@ -159,18 +124,19 @@ def send_mass_mail(email_list):
         return {"Send invoices email : Il n'y a rien à envoyer"}
 
     try:
-        for email_to_send in email_list:
-            mail_to, subject, email_text, email_html, context, attachement_file_list = (
-                email_to_send
-            )
-            send_mail(
-                mail_to,
-                subject,
-                email_text,
-                email_html,
-                context,
-                attachement_file_list,
-            )
+        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+        server.starttls(context=ssl.create_default_context())
+        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+
+        for emails_slice in iter_slice(email_list, 50):
+            server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+            server.starttls(context=ssl.create_default_context())
+            server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+
+            for email_to_send in emails_slice:
+                send_mail(server, *email_to_send)
+
+            server.close()
 
     except (smtplib.SMTPException, ValueError) as error:
         raise EmailException("Erreur envoi email") from error
@@ -178,7 +144,7 @@ def send_mass_mail(email_list):
     return {"Send invoices email : ", f"{len(email_list)} ont été envoyés"}
 
 
-def send_mail(mail_to, subject, email_text, email_html, context, attachement_file_list):
+def send_mail(server, mail_to, subject, email_text, email_html, context, attachement_file_list):
     """Envoi le mail avec le template souhaité"""
     message = MIMEMultipart()
     body = MIMEMultipart("alternative")
@@ -204,7 +170,7 @@ def send_mail(mail_to, subject, email_text, email_html, context, attachement_fil
     # Mise en place de la signature DKIM
     dkim_file = Path(ENV_ROOT).parent / DKIM_PEM_FILE
 
-    with dkim_file.open() as pem_file, SmtpServer() as smtp:
+    with dkim_file.open() as pem_file:
         dkim_private_key = pem_file.read()
         sig = dkim.sign(
             message=message.as_bytes(),
@@ -215,52 +181,5 @@ def send_mail(mail_to, subject, email_text, email_html, context, attachement_fil
             include_headers=[b"from", b"to"],
         ).decode()
         message["DKIM-Signature"] = sig.lstrip("DKIM-Signature: ")
-        smtp.server_mail.sendmail(EMAIL_HOST_USER, mail_to, message.as_string())
 
-
-if __name__ == "__main__":
-    a = SmtpServer()
-    print("1 ======")
-    print(a.nb_connect())
-    b = SmtpServer()
-    print("2 ======")
-    print(a.nb_connect())
-    print(b.nb_connect())
-
-    print("3 ======")
-    c = SmtpServer()
-    print(a.nb_connect())
-    print(b.nb_connect())
-    print(c.nb_connect())
-
-    print("4 ======")
-    a.quit()
-    print(a.nb_connect())
-    print(b.nb_connect())
-    print(c.nb_connect())
-
-    with SmtpServer() as server:
-        print("5 ======")
-        print(server.nb_connect())
-        print(a.nb_connect())
-        print(b.nb_connect())
-        print(c.nb_connect())
-
-    print("6 ======")
-    print(a.nb_connect())
-    print(b.nb_connect())
-    print(c.nb_connect())
-
-    print("7 ======")
-
-    b.quit()
-    c.quit()
-    print(a.nb_connect())
-    print(b.nb_connect())
-    print(c.nb_connect())
-
-    print("8 ======")
-    a.quit()
-    print(a.nb_connect())
-    print(b.nb_connect())
-    print(c.nb_connect())
+    server.sendmail(EMAIL_HOST_USER, mail_to, message.as_string())
