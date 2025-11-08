8691093f65437b60462f7cbc386eb019f21562eb
Paulo <paulo.alves@4a-info.fr>
2025-11-08 18:42:45 +0100

    programmation iban sur première page de résumé

--- apps/edi/bin/chrono_direct.py
+++ apps/edi/bin/chrono_direct.py
@@ -2,14 +2,61 @@ from pathlib import Path
 import csv
 from decimal import Decimal
 
-from django.db import transaction
-
-from apps.core.functions.functions_setups import settings
-from apps.centers_clients.models import Maison
+import psycopg2
+from psycopg2.extensions import parse_dsn
 
 
-@transaction.atomic
-def set_generique_chrono_direct(file_path, interne_file_path, dte_fac):
+def cnx_postgresql(string_of_connexion):
+    """
+    Fonction de connexion à Postgresql par psycopg2
+        :param string_of_connexion: cnx_string = (
+                                        f"dbname={NAME_DATABASE} "
+                                        f"user={USER_DATABASE} "
+                                        f"password={PASSWORD_DATABASE} "
+                                        f"host={HOST_DATABASE} "
+                                        f"port={PORT_DATABASE}"
+                                    )
+        :return: cnx
+    """
+    try:
+        kwargs_cnx = parse_dsn(string_of_connexion)
+        connexion = psycopg2.connect(**kwargs_cnx)
+
+    except psycopg2.Error as except_error:
+        log_line = f"cnx_postgresql error: {except_error}\n"
+        print(log_line)
+        connexion = None
+
+    return connexion
+
+
+def query_select(cnx, sql_requete=None, params=None):
+    """
+    Fonction qui retourne le resultat d'une requête sql Postgresql.
+        :param cnx: Connexion à la base postgresql psycopg2
+        :param sql_requete: requête selection souhaitée
+                                ex: "SELECT * FROM table"
+        :param params: dictionnaire des paramètes à passer
+        :return: La liste des éléments de la requête
+    """
+    if sql_requete:
+        try:
+            with cnx.cursor() as cur:
+                if params:
+                    cur.execute(sql_requete, params)
+                else:
+                    cur.execute(sql_requete)
+                list_rows = cur.fetchall()
+                # print(cur.mogrify(sql_requete, params).decode())
+            return list_rows
+
+        except psycopg2.Error as except_error:
+            log_line = f"query_select error: {except_error}\n"
+            print(log_line)
+    return None
+
+
+def set_generique_chrono_direct(cnx_string, file_path, interne_file_path, dte_fac):
     """Table pour génération des factures chronodirect"""
 
     base_dict = {
@@ -46,14 +93,18 @@ def set_generique_chrono_direct(file_path, interne_file_path, dte_fac):
         "Commentaire": "",
     }
 
-    reference_dict = {
-        item.reference_cosium: item.cct.cct for item in Maison.objects.using("heron").all()
-    }
-
     with (
+        cnx_postgresql(cnx_string) as cnx,
         file_path.open("r", encoding="cp1252") as file,
         interne_file_path.open("w", encoding="utf8") as interne_file,
     ):
+        query = query_select(
+            cnx,
+            sql_requete='select "reference_cosium", "cct" from "centers_clients_maison"'
+        )
+        reference_dict = {
+            item[0]: item[1] for item in query
+        }
         csv_reader = csv.reader(file, delimiter=";", quotechar='"')
         interne_writer = csv.writer(
             interne_file,
@@ -69,7 +120,7 @@ def set_generique_chrono_direct(file_path, interne_file_path, dte_fac):
             mont_cession = Decimal(line[4].replace(",", "."))
 
             if reference_dict.get(maison) is None:
-                raise Exception(
+                raise ValueError(
                     f"La référence cosium dans les client n'exite pas({maison})"
                 )
 
@@ -105,8 +156,15 @@ if __name__ == "__main__":
     date_facture = "20250930"
     # =================================================================================
 
+    cnx_string = (
+        "dbname=heron "
+        "user=heron "
+        "password=heron "
+        "host=10.9.2.109 "
+        "port=5439"
+    )
     file_path = Path(rf"/Users/paulo/Downloads/{nom_fichier_info}")
     interne_file_path = Path(
         rf"/Users/paulo/Downloads/generique_interne_chronodirect_{date_facture}.csv"
     )
-    set_generique_chrono_direct(file_path, interne_file_path, dte_fac=date_facture)
+    set_generique_chrono_direct(cnx_string, file_path, interne_file_path, dte_fac=date_facture)
