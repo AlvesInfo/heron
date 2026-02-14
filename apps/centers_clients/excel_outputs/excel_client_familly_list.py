6a50d5e3894290a46ac2d195dbf03e345445dea0
Paulo <paulo.alves@4a-info.fr>
2026-02-14 12:24:26 +0100

    ajout des vues pour les familles clients

--- apps/centers_clients/excel_outputs/excel_client_familly_list.py
+++ apps/centers_clients/excel_outputs/excel_client_familly_list.py
@@ -71,11 +71,11 @@ def get_clean_rows():
 
 def excel_client_familly(file_io: io.BytesIO, file_name: str) -> dict:
     """Fonction de génération du fichier de liste Familles Clients"""
-    list_excel = [file_io, ["LISTE FAMILLES CLIENTS"]]
+    list_excel = [file_io, ["LISTE CATEGORIES CLIENTS"]]
     excel = GenericExcel(list_excel)
 
     try:
-        titre_page_writer(excel, 1, 0, 0, columns, "LISTE DES FAMILLES CLIENTS")
+        titre_page_writer(excel, 1, 0, 0, columns, "LISTE DES CATEGORIES CLIENTS")
         output_day_writer(excel, 1, 1, 0)
         columns_headers_writer(excel, 1, 3, 0, columns)
         f_lignes = [dict_row.get("f_ligne") for dict_row in columns]
